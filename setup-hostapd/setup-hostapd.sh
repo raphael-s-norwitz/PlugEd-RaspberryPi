4a376eade839310f3b80aca51810620e934660e5
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2017-07-30 22:47:03 +0000

    line replacement works

--- setup-hostapd/setup-hostapd.sh
+++ setup-hostapd/setup-hostapd.sh
@@ -2,13 +2,15 @@
 
 # this script automates the setup process for hostapd on a raspberry pi 3
 
-#@author Raphael Norwitz 
-# email: raphael.s.norwitz@gmail.com
-# github:raphael-s-norwitz
+# @author Raphael Norwitz 
+#  email: raphael.s.norwitz@gmail.com
+#  github:raphael-s-norwitz
 
 instructions="https://cdn-learn.adafruit.com/downloads/pdf/setting-up-a-raspberry-pi-as-a-wifi-access-point.pdf"
 # modified files
 dhcpconf="/etc/dhcp/dhcpd.conf"
+dhcpconfbak="$dhcpconf.bak"
+dhcpconftmp="$dhcpconf.tmp"
 
 # Check key info
 
@@ -30,7 +32,7 @@ fi
 # fix dhcp configuration file
 
 # first backup dhcp conf 
-cp $dhcpconf $dhcpconf.bak
+#cp $dhcpconf $dhcpconf.bak
 
 # these lines need to be swapped out from default configs
 dhcprepone="option domain-name \"example.org\";"
@@ -50,17 +52,25 @@ echo "$dhcprepthree"
 echo "$dhcprepthreeto"
 
 # get lines to be swaped
-numdhcprone=$(grep -n $dhcprepone $dhcpconf | awk -F: 'NR==1{print $1}')
-numdhcprtwo=$(grep -n $dhcpreptwo $dhcpconf | awk -F: 'NR==1{print $1}')
-numdhcprthree=$(grep -n $dhcprepthree $dhcpconf | awk -F: 'NR==1{print $1}') 
+numdhcprone=$(grep -n "$dhcprepone" $dhcpconf | awk -F: 'NR==1{print $1}')
+numdhcprtwo=$(grep -n "$dhcpreptwo" $dhcpconf | awk -F: 'NR==1{print $1}')
+numdhcprthree=$(grep -n "$dhcprepthree" $dhcpconf | awk -F: 'NR==1{print $1}') 
+
+echo "$numdhcprone"
+echo "$numdhcprtwo"
+echo "$numdhcprthree"
 
 # replace lines
-sed -i '/$numdhcprone/c/$dhcpreponeto' $dhcpconf
-sed -i '/$numdhcprtwo/c/$dhcpreptwoto' $dhcpconf
-sed -i '/$numdhcprthree/c/$dhcprepthreeto' $dhcpconf
+awk -v num="$numdhcprone" -v to="$dhcpreponeto" 'NR==num {$0=to} 1' $dhcpconfbak > $dhcpconf
+cat $dhcpconf > $dhcpconftmp
+awk -v num="$numdhcprtwo" -v to="$dhcpreptwoto" 'NR==num {$0=to} 1' $dhcpconftmp > $dhcpconf
+cat $dhcpconf > $dhcpconftmp
+awk -v num="$numdhcprthree" -v to="$dhcprepthreeto" 'NR==num {$0=to} 1' $dhcpconftmp > $dhcpconf
+rm $dhcpconftmp
+
 
 # append subnet to dhcpd.conf
-cat dhcp_subnet.txt >> $dhcpconf
+#cat dhcp_subnet.txt >> $dhcpconf
 
 # First run checks
 
