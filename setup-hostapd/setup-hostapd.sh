193efef7ba633ec3b3950680e43f1104914a3909
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2017-07-30 19:46:22 -0700

    got to network configuration replacement

--- setup-hostapd/setup-hostapd.sh
+++ setup-hostapd/setup-hostapd.sh
@@ -6,11 +6,31 @@
 #  email: raphael.s.norwitz@gmail.com
 #  github:raphael-s-norwitz
 
+# args
+apinterface="wlan0"
+frominterface="eth0"
+apipaddr="192.168.42.1"
+apnetmask="255.255.255.0"
+
+
+# manual instructions
 instructions="https://cdn-learn.adafruit.com/downloads/pdf/setting-up-a-raspberry-pi-as-a-wifi-access-point.pdf"
+
 # modified files
 dhcpconf="/etc/dhcp/dhcpd.conf"
+iscdhcpconf="/etc/default/isc-dhcp-server"
+netconf="/etc/network/interfaces"
+
+# create backups
 dhcpconfbak="$dhcpconf.bak"
-dhcpconftmp="$dhcpconf.tmp"
+iscdhcpconfbak="$iscdhcpconf.bak"
+netconfbak="$netconf.bak"
+
+# backup dhcp configurations
+# cp $dhcpconf $dhcpconf.bak
+# cp $iscdhcpconf $iscdhcpconfbak
+# cp $netconf $netconfbak
+
 
 # Check key info
 
@@ -23,16 +43,15 @@ if [ "$priv" != "root" ]; then
 fi
 
 # update system
-apt-get update -y
+# apt-get update -y
 # install dependencies
-apt-get install -y hostapd isc-dhcp-server
+# apt-get install -y hostapd isc-dhcp-server
 # CHECK THIS (may not work out the box)
-apt-get install -yes -force-yes iptables-persistent
+# apt-get install -yes -force-yes iptables-persistent
 
 # fix dhcp configuration file
 
 # first backup dhcp conf 
-cp $dhcpconf $dhcpconf.bak
 
 # these lines need to be swapped out from default configs
 dhcprepone="option domain-name \"example.org\";"
@@ -51,27 +70,172 @@ echo "$dhcpreptwoto"
 echo "$dhcprepthree"
 echo "$dhcprepthreeto"
 
-# get lines to be swaped
-numdhcprone=$(grep -n "$dhcprepone" $dhcpconf | awk -F: 'NR==1{print $1}')
-numdhcprtwo=$(grep -n "$dhcpreptwo" $dhcpconf | awk -F: 'NR==1{print $1}')
-numdhcprthree=$(grep -n "$dhcprepthree" $dhcpconf | awk -F: 'NR==1{print $1}') 
 
-echo "$numdhcprone"
-echo "$numdhcprtwo"
-echo "$numdhcprthree"
+iscdhcprep="INTERFACES=\"\""
+iscdhcprepto="INTERFACES=\"$apinterface\""
 
-# replace lines
-awk -v num="$numdhcprone" -v to="$dhcpreponeto" 'NR==num {$0=to} 1' $dhcpconfbak > $dhcpconf
-cat $dhcpconf > $dhcpconftmp
-awk -v num="$numdhcprtwo" -v to="$dhcpreptwoto" 'NR==num {$0=to} 1' $dhcpconftmp > $dhcpconf
-cat $dhcpconf > $dhcpconftmp
-awk -v num="$numdhcprthree" -v to="$dhcprepthreeto" 'NR==num {$0=to} 1' $dhcpconftmp > $dhcpconf
-rm $dhcpconftmp
+# first param: line to replace
+# second param: file to replace it in
+# should be ONLY 1 occurance in file
+replace_line_string () {
+	if [ -z "$1" ];
+	then
+		echo "missing args"
+		return 1
+	elif [ -z "$2" ];
+	then
+		echo "missing second arg"
+		return 1
+	fi
 
+	# args
+	toreplace="$1"
+	file="$2"
+	newline="$3"
+	filetmp="$2.shcp.tmp"
+
+	# get line number
+	linenum=$(grep -n "$1" $2 | awk -F: 'NR==1{print $1}')
+
+	# copy file
+	cp $file $filetmp
+	
+	# replace line and go to original file
+	awk -v num="$linenum" -v to="$newline" 'NR==num {$0=to} 1' $filetmp > $file
+
+	# remove intermediate file
+	rm $filetmp
+
+	return 0
+}
+
+# first is the string to find
+# sencond if the file
+get_line () {
+	if [ -z "$1" ];
+	then
+		echo "missing args"
+		return 1
+	elif [ -z "$2" ];
+	then
+		echo "missing second arg"
+		return 1
+	fi
+	
+	tofind="$1"
+	file="$2"
+
+	# find line number
+	grep -n "$tofind" $file | awk -F: 'NR==1{print $1}'
+	
+	return 0
+}
+
+# put value in front
+prepend_line () {
+	repstr="#"
+
+	if [ -z "$1" ];
+	then
+		echo "missing line number"
+		return 1
+	elif [ -z "$2" ];
+	then
+		echo "missing file"
+		return 1
+	elif [ -z "$3" ];
+	then
+		repstr="$3"
+	fi
+
+	line="$2"
+	file="$1"
+	filetmp="$1.tmp"
+
+	# get tmp
+	cp $file $filetmp
+
+	awk -v linenum="$line" -v rstr="$repstr" 'NR==linenum {$0=rstr$0} 1' $filetmp > $file
+	
+	rm $filetmp
+
+	return 0
+}
+
+prepend_everything_after () {
+	repstr="#"
+
+	if [ -z "$1" ];
+	then
+		echo "missing line number"
+		return 1
+	elif [ -z "$2" ];
+	then
+		echo "missing file"
+		return 1
+	elif [ -z "$3" ];
+	then
+		repstr="$3"
+	fi
+
+	line="$2"
+	file="$1"
+	filetmp="$1.tmp"
+
+	# get tmp
+	cp $file $filetmp
+
+	awk -v linenum="$line" -v rstr="$repstr" 'NR >= linenum {$0=rstr$0} 1' $filetmp > $file
+	
+	rm $filetmp
+
+	return 0
+}
+
+
+
+
+
+
+# replace lines from dhcp conf
+# replace_line_string $dhcprepone $dhcpconf $dhcprepone
+# replace_line_string $dhcpreptwo $dhcpconf $dhcpreptwo
+# replace_line_string $dhcprepthree $dhcpconf $dhcprepthree
 
 # append subnet to dhcpd.conf
-cat dhcp_subnet.txt >> $dhcpconf
+# cat dhcp_subnet.txt >> $dhcpconf
 
-# First run checks
+# test replace line function
+# replace_line_string newline ./test.txt works
+# replace_line_string works ./test.txt newline
 
+# add configurations to isc-dhcp-server
+# replace_line_string $iscdhcprep $iscdhcpconf $iscdhcprepto
+
+# take wi-fi down
+# sudo ifdown $apinterface
+
+
+# test get line and prepend functions
+#linez=$(get_line second ./test.txt)
+#echo $linez
+# echo "---"
+#cat ./test.txt
+#echo "---"
+#prepend_line ./test.txt $linez \#
+#cat ./test.txt
+#echo "---"
+#prepend_everything_after ./test.txt $linez \#
+#cat ./test.txt
+
+
+# edit network interfaces
+intfacenetline=$(get_line $apinterface $netconf)
+prepend_everything_after $netconf $intfacenetline \#
+
+echo "allow-hotplug $apinterface" >> $netconf
+echo "iface $apinterface inet static" >> $netconf
+echo " address $apipaddr" >> $netconf
+echo " netmast $apnetmast" >> $netconf
+echo ---
 
