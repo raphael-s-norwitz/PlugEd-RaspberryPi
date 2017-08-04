cd28775db1022d4cba6f2e3a0f533564acfd40be
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2017-08-03 22:11:07 -0700

    added setup for kalite

---
+++ kalite/setup_kalite.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# update system
+sudo apt-get update
+
+# install dependencies
+sudo apt-get install python-m2crypto python-pkg-resources nginx python-psutil
+
+# Fetch the latest .deb
+sudo wget https://learningequality.org/r/deb-pi-installer-0-17 --no-check-certificate --content-disposition
+
+# Install the .deb
+sudo dpkg -i ka-lite-raspberry-pi*.deb
