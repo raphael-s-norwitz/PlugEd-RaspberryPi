92d6381d3d58c7a330d6fdfbd41c2c37f821cd72
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2017-08-06 01:27:25 -0700

    added buttons and redirect to kalite

--- pluged_app/pluged_app.py
+++ pluged_app/pluged_app.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 
 @app.route('/')
 def pluged_homepage():
-    return render_template('pluged_page.html')
+	return render_template('pluged_page.html')
+
+@app.route('/kanav')
+def kalite_nav(ip="192.168.42.1", port=4848):
+	return redirect("http://" + ip + ":" + str(port), code=302)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.debug = bool(os.environ.get('PRODUCTION'))
